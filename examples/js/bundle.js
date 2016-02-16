a8782344200df2c384476fdfe0d965950d0a07cd
Martin Thorsen Ranang <mtr@ranang.org>
2016-02-16 09:08:03 +0100

    Update 2016-02-16T08:08:01.769Z

--- examples/js/bundle.js
+++ examples/js/bundle.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
- * @license core-layout-probe v5.5.1, 2016-02-15T15:27:59+0100
+ * @license core-layout-probe v5.5.2, 2016-02-16T09:07:37+0100
  * (c) 2016 Martin Thorsen Ranang <mtr@ranang.org>
  * License: MIT
  */
@@ -280,7 +280,7 @@
             html.style.height = window.innerHeight + 'px';
         }
 
-        window.addEventListener('resize', _.debounce(_onResize, 125, {
+        angular.element(window).on('resize', _.debounce(_onResize, 125, {
             leading: true,
             maxWait: 250,
             trailing: true
@@ -63305,8 +63305,8 @@ module.exports = angular
     .module('myApp.version', [
         require('./version.directive.js').name
     ])
-    .value('version', '5.5.1')
-    .value('buildTimestamp', '2016-02-15T15:28:03+0100');
+    .value('version', '5.5.2')
+    .value('buildTimestamp', '2016-02-16T09:07:41+0100');
 
 },{"./version.directive.js":16,"angular-x":7}],18:[function(require,module,exports){
 'use strict';
